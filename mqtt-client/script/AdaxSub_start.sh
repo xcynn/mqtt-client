251d09532023212dec719bfb0c79e75f676373d9
XU CHENGYANG <xucy@i2r.a-star.edu.sg>
2014-04-23 17:18:00 +0800

    separate out SnS sub/pub implementation

--- mqtt-client/script/AdaxSub_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /usr/local/mqtt-client/mqtt-client/
-java -cp ./target/mqtt-client-1.10-SNAPSHOT-uber.jar org.fusesource.mqtt.cli.AdaxSubscriber
--- mqtt-client/script/PubStaNodeDemo_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /usr/local/mqtt-client/mqtt-client/
-java -cp ./target/mqtt-client-1.10-SNAPSHOT-uber.jar org.fusesource.mqtt.cli.PublisherStaticNodeDemo -h tcp://10.217.138.185:1883 -q 2 -t /Demo/ -m "Demo" -n 10 -s 500 -itr 10000 -d
